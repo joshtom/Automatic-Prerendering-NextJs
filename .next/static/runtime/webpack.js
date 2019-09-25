b72cbdbf07b3f7add8a8646f818d45bca166aaa3
Joshua <olajidejoshua4real@gmail.com>
2019-09-25 10:18:21 -0400

    New folder and file created inside pages file

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a55a190fefb4d2d5beb";
+/******/ 	var hotCurrentHash = "dc7a959c211ca53957a5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
