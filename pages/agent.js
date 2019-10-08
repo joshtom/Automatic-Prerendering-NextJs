b93b65940444bed4539b71593c9eba8e4c1ba708
Joshua <olajidejoshua4real@gmail.com>
2019-10-08 12:13:36 +0100

    code that uses SSR to retrieve the user agent added

--- pages/agent.js
+++ pages/agent.js
@@ -1,9 +1,10 @@
 const Agent = ({ userAgent }) => {
     <h1> Your user agent is: {userAgent} </h1>
+};
     Agent.getInitialProps = async ({ req }) => {
         const userAgent = req ? req.headers['user-agent'] :  navigator.userAgent;
         return { userAgent };
     };
-};
 
-export default Agent
+
+export default Agent;
